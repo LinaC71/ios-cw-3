65d8b2a3e396401c2fb7bad6b8450359c6b05413
LinaC71 <Linaalosaimi155@gmail.com>
2020-09-16 20:25:34 +0300

    done!!!

---
+++ cw3-p1.playground/Contents.swift
@@ -0,0 +1,14 @@
+func PrintMe(){
+    print("Hi, My name is Lina")
+    print("I'm sixteen years old")
+    print("I'm in the eleventh grade")
+}
+PrintMe()
+
+func Howtoeatchocolate() {
+    print("Buy a chocolate of your choice")
+    print("Remove the package from the chocolate")
+    print("Take a bite of the chocolate")
+    print("Enjoy!!!")
+}
+Howtoeatchocolate()
---
+++ cw3-p2.playground/Contents.swift
@@ -0,0 +1,9 @@
+var number = 3
+var number2 = 5
+func add(a: Int, b: Int) -> Int {
+    return (a+b)
+
+}
+   var function1 = add(a: 5, b: 9)
+print(function1)
+
